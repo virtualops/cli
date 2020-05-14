cafd927c19fde2921fe701de04d1cd33796658be
Leo Sjöberg <leo.sjoberg@gmail.com>
2020-05-14 18:33:05 +0300

    add proper release workflow

--- cmd/deploy.go
+++ cmd/deploy.go
@@ -7,6 +7,9 @@ import (
 
 var deployCmd = &cobra.Command{
 	Use: "deploy",
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		// for now we can ignore the breeze.yaml check since the command doesn't exist
+	},
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("Unfortunately, Breeze isn't yet ready for production releases.")
 		fmt.Println("You can sign up for updates on Breeze for production at https://breeze.sh")
